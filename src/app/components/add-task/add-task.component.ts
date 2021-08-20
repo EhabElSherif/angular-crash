26e9b6d357cc0cd4a49aa0de1818191d9a290dd3
Ehab El-Sherif <ehabraelsherif@gmail.com>
2021-08-20 16:45:08 +0200

    toggle showAddForm

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,3 +1,5 @@
+import { Subscription } from 'rxjs';
+import { UiService } from './../../services/ui.service';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Task } from 'src/app/Task';
 
@@ -12,7 +14,12 @@ export class AddTaskComponent implements OnInit {
   day = ""
   reminder = false
 
-  constructor() { }
+  showForm: boolean|undefined
+  subscription: Subscription
+  
+  constructor(private uiService: UiService) { 
+    this.subscription = this.uiService.onToggle().subscribe(value=>this.showForm = value)
+  }
 
   ngOnInit(): void {
   }
